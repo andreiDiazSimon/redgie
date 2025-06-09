d45106171c5881e6c2ea1101897bf661b29fe3fe
andreiDiazSimon <andreisimon2004@gmail.com>
2025-06-09 18:59:31 +0800

    gawing 3 seconds

--- hehe.lua
+++ hehe.lua
@@ -5,5 +5,5 @@ local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 
 for i = 1, 10 do
     Character:FindFirstChildOfClass("Humanoid"):ChangeState(Enum.HumanoidStateType.Jumping)
-    task.wait(1) -- wait 1 second between jumps
+    task.wait(3) -- wait 1 second between jumps
 end
